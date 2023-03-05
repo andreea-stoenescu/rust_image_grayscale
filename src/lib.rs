030356ce11fd79073e0859d1b62cc192ffec52d2
Andreea <andreea.stoenescu@hotmail.com>
2023-03-05 11:47:34 +0000

    use the image dependency and load the image from memory

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
 use wasm_bindgen::prelude::*;
 use web_sys::console::log_1 as log;
+use base64::decode;
+use image::load_from_memory;
 
 #[wasm_bindgen]
 pub fn grayscale (encoded_file: &str) {
-    log(&encoded_file.into());
+    log(&"Grayscale called".into());
+
+    let base64_to_vector = decode(encoded_file).unwrap();
+    log(&"image decoded".into());
+
+    let img = load_from_memory(&base64_to_vector).unwrap();
+    log(&"Image loaded".into());
 }
